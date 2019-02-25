44fcdc006e713cb0be6cadd587e43dcdbe1c38e8
Andrei Chirila <andreich@gmail.com>
2019-02-25 23:35:36 +0100

    Update travis - coveralls integration.

--- coverage.sh
+++ coverage.sh
@@ -5,12 +5,13 @@ REPORT="go tool cover -html=gover.coverprofile"
 if [[ $1 == "travis-ci" ]]; then
   GOVER=$HOME/gopath/bin/gover
   GOVERALLS=$HOME/gopath/bin/goveralls
-  REPORT="$GOVERALLS -coverprofile=gover.coverprofile"
+  REPORT="$GOVERALLS -service=travis-ci -coverprofile=gover.coverprofile"
 fi
 
-for I in `go list ./...`; do
-  go test $I -covermode=$MODE -coverprofile=$(basename $I).coverprofile;
-done
+go test ./... -covermode=$MODE -coverprofile=gover.coverprofile;
 
-$GOVER
-$REPORT
+if [[ ! -z "$GOVER" ]]; then
+  $GOVER
+fi
+
+$REPORT 

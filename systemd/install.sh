060bee7de6686f9f38616b80777fede194c2ec24
Andrei Chirila <andreich@gmail.com>
2019-03-03 20:47:57 +0100

    Add script for installing docsync as systemd service.

---
+++ systemd/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DOCSYNC=`which docsync`
+
+if [[ -z "$DOCSYNC" ]]; then
+    echo "docsync not found in \$PATH"
+    echo "Consider installing it with:"
+    echo "$ go install github.com/andreich/docsync"
+    exit 1
+fi
+
+sudo tee /etc/systemd/system/docsync-${USER}.service <<EOF
+[Unit]
+Description=docsync service - http://github.com/andreich/docsync
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=${USER}
+ExecStart=${DOCSYNC} -config /home/${USER}/.docsync/config.json -dry_run=false
+
+[Install]
+WantedBy=multi-user.target
+EOF

4b482d08cfaad75cbea0ba434996507f7f247927
Andrei Chirila <andreich@gmail.com>
2019-03-03 13:00:21 +0100

    Update coverage and travis configuration.

--- coverage.sh
+++ coverage.sh
@@ -5,10 +5,10 @@ REPORT="go tool cover -html=gover.coverprofile"
 if [[ $1 == "travis-ci" ]]; then
   GOVER=$HOME/gopath/bin/gover
   GOVERALLS=$HOME/gopath/bin/goveralls
-  REPORT="$GOVERALLS -service=travis-ci -coverprofile=gover.coverprofile"
+  REPORT="$GOVERALLS -service=travis-ci -repotoken=${COVERALLS_TOKEN} -coverprofile=gover.coverprofile"
 fi
 
-go test ./... -covermode=$MODE -coverprofile=gover.coverprofile;
+go test ./... -covermode=$MODE -coverprofile=gover.coverprofile
 
 if [[ ! -z "$GOVER" ]]; then
   $GOVER
